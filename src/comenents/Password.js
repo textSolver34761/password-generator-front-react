a045a72ee3b619b4b1f3500b6e2328036dd077a2
textSolver34761 <pradonbenjamin@gmail.com>
2022-06-05 22:24:17 +0200

    update

--- src/comenents/Password.js
+++ src/comenents/Password.js
@@ -1,31 +1,37 @@
-function Password(props) {
-    return(
-    <div>
-        <h1>{props.h1}</h1>
-        <div>
-            <input type="checkbox" id="symbols" name="symbols" value="symbols" />
-            Symbols (&, !, ? ...)
-        </div>
-        <div>
-            <input type="checkbox" id="capital" name="capital" value="capital" />
-            Capital letter (A, C, D, ...)
-        </div>
-        <div>
-            <input type="checkbox" id="small" name="small" value="small"/>
-            Small letter (a, c, d ...)
-        </div>
-        <div>
-          <div>password lengh :</div>
-          <div> choose beeween 1 and 12 characters </div>
-          <input type="range" min={1} max={12} />
-        </div>
-        <button className="btn btn-alerte" onClick={getdata}>GENERATE</button>
-    </div>
-    )
-}
+import React from "react";
 
-function getdata() {
+function Password(props) {
     
+    function handleChange(e) {
+        const checked = e.target.value;
+        console.log(checked);
+    };
+
+  return (
+    <div>
+      <h1>{props.h1}</h1>
+      <div>
+        <input type="checkbox" id="symbols" name="symbols" value="symbols" onChange={e => this.handleChange.bind(e)} />
+        Symbols (&, !, ? ...)
+      </div> 
+      <div>
+        <input type="checkbox" id="capital" name="capital" value="capital" />
+        Capital letter (A, C, D, ...)
+      </div>
+      <div>
+        <input type="checkbox" id="small" name="small" value="small" />
+        Small letter (a, c, d ...)
+      </div>
+      <div>
+        <div>password lengh :</div>
+        <div> choose beeween 1 and 12 characters </div>
+        <input type="range" min={1} max={12} />
+      </div>
+      <button className="btn btn-alerte" onClick={handleChange}>
+        GENERATE
+      </button>
+    </div>
+  );
 }
 
 export default Password;
